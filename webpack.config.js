22d6046bd9cb7a8dda3c570d84fb8802c1310877
Michael Kim <micklepickle98@gmail.com>
2020-11-15 02:18:22 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const config = {
+  entry: {
+    main: './src/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: '>1%, not ie 11, not op_mini all'
+                }
+              ],
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./src/index.html')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets', to: 'assets' }]
+    })
+  ],
+  stats: {
+    colors: true
+  }
+};
+
+module.exports = async (env, argv) => {
+  if (argv.mode === 'production') {
+    config.mode = 'production';
+    // Basic options, except ignore console statements
+    config.optimization = {
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+          terserOptions: {
+            compress: {
+              drop_console: true
+            }
+          }
+        })
+      ]
+    };
+  } else {
+    config.mode = 'development';
+    config.devtool = 'cheap-module-source-map';
+    config.devServer = {
+      contentBase: path.resolve('./build'),
+      host: 'localhost',
+      port: '8080',
+      hot: true,
+      overlay: true,
+      writeToDisk: true
+    };
+  }
+
+  return config;
+};
